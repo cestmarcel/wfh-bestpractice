3a8d4af1a5b789947a05ba9ffe8e143a2df309ac
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-05-16 19:44:16 -0400

    Initial setup of animations on landing page

--- scripts.js
+++ scripts.js
@@ -17,4 +17,30 @@ $(document).ready(function() {
         800 //speed
       );
     });
+
+  var $animation_elements = $('.animate__animated');
+
+  function checkInView() {
+    var window_height = $(window).height();
+    var window_top_position = $(window).scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+  
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+  
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+          (element_top_position <= window_bottom_position)) {
+        $element.addClass($element.data("animation"));
+      } else {
+        $element.removeClass($element.data("animation"));
+      }
+    });
+  }
+
+  $(window).on('scroll', checkInView);
+
   });
