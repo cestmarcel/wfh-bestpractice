a1d3a5d6215d3c4140569de99a9021da86a5db40
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-05-17 12:59:27 -0400

    Remove background-attachment on mobile

--- scripts.js
+++ scripts.js
@@ -13,6 +13,11 @@ $(document).ready(function() {
 
   $(window).scroll(function(){
     var scroll_position = $(window).scrollTop()/2;
+
+    if (window.innerWidth < 785) {
+      scroll_position = scroll_position / 4;
+    }
+
     $('.header-parallax').css({
         'background-position-y' : - scroll_position + "px",
     })
